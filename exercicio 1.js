89844455f4d9d179aeb13d2da5745189d23a6f32
Vinidev1295 <vinidias1295@gmail.com>
2024-10-01 11:27:27 -0300

    Create exercicio 1.js

---
+++ exercicio 1.js
@@ -0,0 +1,6 @@
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+//a variável i=0, vai entrar em loop incrementando 1 a cada loop até o número ser menor que 5 pois i<5 e depois vai somar todos os loops resultando em 10.//
