f906afd3c97ec40ec48e505a19d43a7c221586b1
Vinidev1295 <vinidias1295@gmail.com>
2024-10-01 11:45:07 -0300

    Create exercicio 2.s

---
+++ exercicio 2.s
@@ -0,0 +1,10 @@
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+/*a) O que vai ser impresso no console?
+Será impresso 19,21,23,25,27,30
+b) Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...` é suficiente? Se sim, o que poderia ser usado para fazer isso?
+Sim, seria. Para acessar índice de cada elemento da lista você precisar espeficiar qual elemento deseja acessar no console.log;Exemplo: console.log(numero[1]).
