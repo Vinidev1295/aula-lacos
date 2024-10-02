bd032d5f9adb3b3e280d096021726c593f72f5cd
Vinidev1295 <vinidias1295@gmail.com>
2024-10-02 10:40:26 -0300

    Create exercicio 4.js

---
+++ exercicio 4.js
@@ -0,0 +1,11 @@
+let pet = Number(prompt("Quantos bichos de estimação você tem? "))
+let array = []
+if(pet===0){
+  console.log("Que pena! Você pode adotar um pet!")
+}else{
+ for(let i=0; i<pet; i++){
+ pet2 = prompt("Qual o nome dos seus pets? ")
+      array.push(pet2)
+}
+  console.log(array)
+}
