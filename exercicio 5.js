17f96232a5e60df98e409b25fafbc26903324561
Vinidev1295 <vinidias1295@gmail.com>
2024-10-02 11:19:47 -0300

    Create exercicio 5.js

---
+++ exercicio 5.js
@@ -0,0 +1,17 @@
+//Exercicio a)
+const arrayOriginal = [1,2,3,4,5,6,7,8,9]
+function teste(arrayOriginal){
+    for(let i=0; i<arrayOriginal.length; i++){
+        let receb = arrayOriginal[i]
+        console.log(receb)
+}
+}
+teste(arrayOriginal)
+//Exercicio b)
+function teste2(arrayOriginal){
+    for(let i=0; i<arrayOriginal.length; i++){
+        let receb2 = arrayOriginal[i]/10
+        console.log(receb2)
+}
+}
+teste2(arrayOriginal)
