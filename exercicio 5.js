8ac06f6ac332d916d470e772ed8f1d0006324b34
Vinidev1295 <vinidias1295@gmail.com>
2024-10-02 11:56:00 -0300

    Update exercicio 5.js

--- exercicio 5.js
+++ exercicio 5.js
@@ -1,17 +1,30 @@
-//Exercicio a)
+//Exercicio A)
 const arrayOriginal = [1,2,3,4,5,6,7,8,9]
-function teste(arrayOriginal){
+function array1(arrayOriginal){
     for(let i=0; i<arrayOriginal.length; i++){
         let receb = arrayOriginal[i]
         console.log(receb)
 }
 }
-teste(arrayOriginal)
-//Exercicio b)
-function teste2(arrayOriginal){
+array1(arrayOriginal)
+
+//Exercicio B)
+function array2(arrayOriginal){
     for(let i=0; i<arrayOriginal.length; i++){
         let receb2 = arrayOriginal[i]/10
         console.log(receb2)
 }
 }
-teste2(arrayOriginal)
+array2(arrayOriginal)
+
+//Exercicio C)
+arrayNovo = []
+function array3(arrayOriginal){
+    for(let i=0; i<arrayOriginal.length; i++){
+        if(arrayOriginal[i]%2===0){
+            arrayNovo.push(arrayOriginal[i])
+}
+}
+console.log(arrayNovo)
+}
+array3(arrayOriginal)
