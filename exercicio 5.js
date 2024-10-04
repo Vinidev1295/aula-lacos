c71cd89ea654a5dbf5258ef48fef79427297e4d7
Vinidev1295 <vinidias1295@gmail.com>
2024-10-04 09:50:14 -0300

    Update exercicio 5.js

--- exercicio 5.js
+++ exercicio 5.js
@@ -18,7 +18,7 @@ function array2(arrayOriginal){
 array2(arrayOriginal)
 
 //Exercicio C)
-arrayNovo = []
+let arrayNovo = []
 function array3(arrayOriginal){
     for(let i=0; i<arrayOriginal.length; i++){
         if(arrayOriginal[i]%2===0){
@@ -28,3 +28,29 @@ function array3(arrayOriginal){
 console.log(arrayNovo)
 }
 array3(arrayOriginal)
+//Exercicio D)
+let arrayNovo2 = ["Arroz", "Feijao", "Batata", "Ervilha"]
+function array4(arrayNovo2){
+    for(let i=0; i<arrayNovo2.length; i++){
+        let receb = arrayNovo2[i]
+    console.log( "O elemento do index", i , "é numero", arrayNovo2[i])
+    }
+}
+array4(arrayNovo2)
+//Exercicio E)
+arrayNumero = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let valorMaximo = 0
+let valorMinimo = 150
+function array5(arrayNumero){
+    for(let i=0; i<arrayNumero.length; i++){
+        let novo = arrayNumero[i]
+        if(novo>valorMaximo){
+            valorMaximo = novo
+        }
+        if(novo<valorMinimo){
+            valorMinimo = novo
+        }
+        }
+    console.log( "O maior valor é", valorMaximo , "o menor valor é", valorMinimo)
+}
+array5(arrayNumero)
