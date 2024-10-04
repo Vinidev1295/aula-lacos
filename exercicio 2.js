c82bd1ef77b0634c4e55112066b7ca7b76a6ac35
Vinidev1295 <vinidias1295@gmail.com>
2024-10-04 09:50:56 -0300

    Update and rename exercicio 2.s to exercicio 2.js

--- exercicio 2.js
+++ exercicio 2.js
@@ -7,4 +7,5 @@ for (let numero of lista) {
 /*a) O que vai ser impresso no console?
 Será impresso 19,21,23,25,27,30
 b) Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...` é suficiente? Se sim, o que poderia ser usado para fazer isso?
-Sim, seria. Para acessar índice de cada elemento da lista você precisar espeficiar qual elemento deseja acessar no console.log;Exemplo: console.log(numero[1]).
+Sim, seria. Para acessar índice de cada elemento da lista você precisar espeficiar qual elemento deseja acessar no console.log;
+Exemplo: console.log(numero[1]).
